9ef8f5096f210664a848e5dcd50213b6354564dd
Ian Gregg <iangregg188@gmail.com>
2020-06-02 16:03:38 -0700

    Updated the definitions of the terms within the html <blacchoy@gmail.com> Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
-  $(".click").click(function() {
-  $(".answer").toggle();
-  })
-})
+// $(document).ready(function() {
+//   $(".click").click(function() {
+//   $(".answer").toggle();
+//   })
+// })
+
+$(document).ready(function(){
+  $(".flashcard").click(function(){
+    $(this).children().toggle();
+  });
+});
